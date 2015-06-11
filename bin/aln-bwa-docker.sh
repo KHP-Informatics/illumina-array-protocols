5c81ed6d851ddbf070884e02794cd2d303eff3bf
rstudio <rstudio@example.com>
2015-06-11 15:56:26 +0100

    README

--- bin/aln-bwa-docker.sh
+++ bin/aln-bwa-docker.sh
@@ -15,8 +15,15 @@ set -o nounset
 array=""
 refGenome=""
 
-bwa mem -t 32 -V -M -a ${refGenome} ${array}.fasta | \
+docker run \
+-w /home/pipeman \
+-e HOME=/home/pipeman \
+-e USER=pipeman \
+--user pipeman \
+-i \
+-t compbio/ngseasy-bwa:1.0 /bin/bash -c \
+"bwa mem -t 32 -V -M -a ${refGenome} ${array}.fasta | \
 samblaster --addMateTags --excludeDups | \
 samtools sort -@ 32 -T temp_ -O sam -o ${array}.sam && \
 samtools index ${array}.sam && \
-rm ${array}.sam
+rm ${array}.sam"
