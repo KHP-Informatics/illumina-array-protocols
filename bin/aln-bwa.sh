41c5adf9565fbc2ccfe6c54f634404180d2eb30c
rstudio <rstudio@example.com>
2015-06-11 15:54:50 +0100

    README

---
+++ bin/aln-bwa.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+########################################################################################################
+# Program: aln-bwa
+# Version 0.1
+# Author: Stephen Newhouse (stephen.j.newhouse@gmail.com);
+###########################################################################################
+
+
+###################################
+## BWA > samblaster > samtools
+#
+
+array=""
+refGenome=""
+
+docker run \
+-w /home/pipeman \
+-e HOME=/home/pipeman \
+-e USER=pipeman \
+--user pipeman \
+-i \
+-t compbio/ngseasy-bwa:1.0 /bin/bash -c \
+"bwa mem -t 32 -V -M -a ${refGenome} ${array}.fasta | \
+samblaster --addMateTags --excludeDups | \
+samtools sort -@ 32 -T temp_ -O sam -o ${array}.sam && \
+samtools index ${array}.sam && \
+rm ${array}.sam"
