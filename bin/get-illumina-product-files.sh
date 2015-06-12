07c680925d520b8d1b47169cae8c893d540334b6
rstudio <rstudio@example.com>
2015-06-12 13:48:13 +0100

    README

--- bin/get-illumina-product-files.sh
+++ bin/get-illumina-product-files.sh
@@ -3,25 +3,6 @@ set -o errexit
 set -o nounset
 
 ## get illumina data for Human* SNP arrays
-for i in PsychArray \
-        HumanCore \
-        HumanCore-24 \
-        HumanCoreExome \
-        HumanCoreExome-24  \
-        HumanCVD  \
-        HumanExome \
-        HumanGenotypingArrays \
-        HumanOmni1-Quad \
-        HumanOmni2-5Exome-8 \
-        HumanOmni5-Quad \
-        HumanOmni5Exome \
-        HumanOmni5MExome \
-        HumanOmni25 \
-        HumanOmniExpress \
-        HumanOmniExpress-24 \
-        HumanOmniExpressExome \
-        HumanOmniZhongHua-8; do
         
-        wget -r -c -b ftp://webdata:webdata@ussd-ftp.illumina.com/Downloads/ProductFiles/${i};
+        wget -r -c -b ftp://webdata:webdata@ussd-ftp.illumina.com/Downloads/ProductFiles/
         
-        done
