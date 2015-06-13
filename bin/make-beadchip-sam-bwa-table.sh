7c4c4d09f6555de86fd418cab453165f338abd93
rstudio <rstudio@example.com>
2015-06-13 08:37:55 +0100

    README & bin/make-beadchip-sam-bwa-table.sh

---
+++ bin/make-beadchip-sam-bwa-table.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+
+echo -e "\n>>>>START [make-beadchip-sam-bwa-table.sh ${1}]\n"   
+
+## Assume awscli installed and set up properly
+## This is set up specifically for our usage
+S3BUCKET="illumina-probe-mappings"
+BUCKET_URL="https://s3-eu-west-1.amazonaws.com/${S3BUCKET}"
+
+## set names and get information
+#SAM=${1}
+SAM="HumanCoreExome-24v1-0_A.sam"
+SAMMD5=`md5sum ${SAM} | awk '{print $1}'`
+SAM_SIZE=`du -h ${SAM} | awk '{print $1}'`
+
+## make beadchip-sam-bwa-table.md
+if [[ ! -e "beadchip-sam-bwa-table.md" ]]; then
+        touch beadchip-sam-bwa-table.md
+        echo -e "| SAM File | Size | MD5 |" >> beadchip-sam-bwa-table.md
+        echo -e "|----------|------|-----|" >> beadchip-sam-bwa-table.md
+fi
+
+## add to table 
+echo -e ".... Updating [beadchip-sam-bwa-table.md]"
+
+    echo -e "| [${SAM}](${BUCKET_URL}/${SAM}) | ${SAM_SIZE} | ${SAMMD5}|" >> beadchip-sam-bwa-table.md
+
+## copy to amazon s3 http://docs.aws.amazon.com/cli/latest/reference/s3/cp.html
+echo -e "\n.... Copying [${SAM}] to amazon s3 : [aws s3 cp ${SAM} s3://${S3BUCKET} --acl public-read]\n"
+
+   aws s3 cp ${SAM} s3://${S3BUCKET} --acl public-read 
+
+   echo -e "\n>>>>DONE [make-beadchip-sam-bwa-table.sh ${1}]\n"   
