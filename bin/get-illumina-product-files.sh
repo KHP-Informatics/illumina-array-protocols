74577c23b135d575cdd91bbe7466333c5a97f7a0
Stephen J Newhouse <stephen.j.newhouse@gmail.com>
2017-02-03 09:19:32 +0000

    updated path CASE changed

--- bin/get-illumina-product-files.sh
+++ bin/get-illumina-product-files.sh
@@ -3,6 +3,5 @@ set -o errexit
 set -o nounset
 
 ## get illumina data for Human* SNP arrays
-        
-        wget -r -c -b ftp://webdata:webdata@ussd-ftp.illumina.com/Downloads/ProductFiles/
-        
+
+wget -r -c -b ftp://webdata:webdata@ussd-ftp.illumina.com/downloads/productfiles/
